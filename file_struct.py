b5c1566aacc26dddb7e22df5b746207d345a6613
Wyatt <duiker2@illinois.edu>
2017-03-29 15:38:08 -0500

    Got shekars stuff

--- file_struct.py
+++ file_struct.py
@@ -1,15 +1,15 @@
 class File:
     global path
     global is_encrypted
-    global key
+    global encrypted_key
 
     def __init__(self, path):
         self.path = path
         self.is_encrypted = False
-        self.key = ""
+        self.encrypted_key = ""
 
     def encrypt(self):
         if not self.is_encrypted:
             # call shekar's encrypt
             self.is_encrypted = True
-            self.key = "TODO shekar sets this"
+            self.encrypted_key = "TODO shekar sets this"
